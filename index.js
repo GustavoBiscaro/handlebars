1b0f0d6b4c78414be02982fbedd18a4bd0ef395f
Gustavo Miguel Oliveira Biscaro <gustavo.biscaro@gmail.com>
2024-02-17 15:10:22 -0300

    preparando o ambiente

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+const hbs = exphbs.create({
+    partialsDir: ["views/partials/"],
+});
+
+app.engine("handlebars", hbs.engine)
+app.set("view engine", "handlebars")
